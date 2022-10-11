4ac437cea75cd3c3da4bd2e20311a00ede64850b
dchaparro@integrupo.com <dchaparro@integrupo.com>
2022-10-11 15:44:07 -0500

    a meaningful commit message

---
+++ VCSBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VCSBasics
+{
+    record Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
